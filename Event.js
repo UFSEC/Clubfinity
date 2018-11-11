79ec0088e480de42c26c0532afb4b5191e8fe8ff
dgarzonmoreno <dgarzonmoreno@ufl.edu>
2018-11-11 02:09:37 -0500

    models added

---
+++ Event.js
@@ -0,0 +1,11 @@
+function Event(name, club_id, location, moi, information,
+eventdate, clubname)
+{
+  this.name = name;
+  this.club_id = club_id;
+  this.location = location;
+  this.moi = moi;
+  this.information = information;
+  this.eventdate = eventdate;
+  this.clubname = clubname;
+}
