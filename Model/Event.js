75fce6bad214d68ce7b7b097f86e4bba322c71d2
dgarzonmoreno <dgarzonmoreno@ufl.edu>
2018-11-11 20:43:39 -0500

    created get functinos for Event, and users_to_events

--- Model/Event.js
+++ Model/Event.js
@@ -1,10 +1,41 @@
 function Event(name, club_id, location, moi, information,eventdate, clubname)
 {
-  this.name = name;
-  this.club_id = club_id;
-  this.location = location;
-  this.moi = moi;
-  this.information = information;
-  this.eventdate = eventdate;
-  this.clubname = clubname;
+    this.name = name;
+    this.club_id = club_id;
+    this.location = location;
+    this.moi = moi;
+    this.information = information;
+    this.eventdate = eventdate;
+    this.clubname = clubname;
+    return
+    {
+      getName: function()
+      {
+        return name;
+      },
+      getClub_Id: function()
+      {
+        return club_id;
+      },
+      getLocation: function()
+      {
+        return location;
+      },
+      getMoi: function()
+      {
+        return moi;
+      },
+      getInformation: function()
+      {
+        return information;
+      },
+      getEventDate: function()
+      {
+        return eventdate;
+      },
+      getClubName: function()
+      {
+        return clubname;
+      }
+    }
 }
