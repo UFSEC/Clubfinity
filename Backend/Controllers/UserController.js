5860abd0856193978034338b14b0612889ab5be2
Pablo Estrada <pablof300@gmail.com>
2019-09-24 10:50:27 -0400

    Added express-validator for input checking and small fixes to user CRUD

--- Backend/Controllers/UserController.js
+++ Backend/Controllers/UserController.js
@@ -1,48 +1,54 @@
-var userDAO = require("../DAO/UserDAO");
+const userDAO = require("../DAO/UserDAO");
+const { validationResult, param, query } = require("express-validator/check");
+
 exports.getAll = (req, res) => {
   console.log("API GET request called for all users");
   userDAO.getAllUsers(result => {
     res.send(result);
   });
 };
+
 exports.get = (req, res) => {
   console.log(`API GET request called for ${req.params.username}`);
   userDAO.getUser(req.params.username, result => {
     if (result) {
       res.json(result);
     } else {
-      res.status(404).send(`User with username ${req.params.username} not found`);
+      res
+        .status(404)
+        .send(`User with username ${req.params.username} not found`);
     }
   });
 };
 
 // Add input validation with external lib
+// Add check for username to match username of userInfo
 exports.update = (req, res) => {
-  const username = req.params;
-  const userInfo = req.query;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(422).json({ errors: errors.array() });
+    return;
+  }
 
-  if (Object.keys(username).length != 0) {
-    const props = Object.keys(userInfo);
-    if (
-      props.length == 6 &&
-      props[0] == "firstName" &&
-      props[1] == "lastName" &&
-      props[2] == "dob" &&
-      props[3] == "email" &&
-      props[4] == "username" &&
-      props[5] == "password"
-    ) {
-      userDAO.updateUser(username.username, {
-        name: { first: userInfo.firstName, last: userInfo.lastName },
-        dob: userInfo.dob,
-        username: userInfo.username,
-        email: userInfo.email,
-        password: userInfo.password
-      });
-      res.status(204).send();
-    } else {
-      res.status(404).send("Insufficient parameters provided");
-    }
+  const params = req.params;
+  const {
+    firstName,
+    lastName,
+    dateOfBirth,
+    email,
+    username,
+    password
+  } = req.query;
+
+  if (params.username) {
+    userDAO.updateUser(params.username, {
+      name: { first: firstName, last: lastName },
+      dateOfBirth: dateOfBirth,
+      email: email,
+      username: username,
+      password: password
+    });
+    res.status(204).send();
   } else {
     res
       .status(404)
@@ -53,29 +59,56 @@ exports.update = (req, res) => {
 };
 
 exports.create = (req, res) => {
-  const params = req.query;
-  if (Object.keys(params).length == 6) {
-    // <=CHANGE THIS LATER I DON'T LIKE MAGIC NUMBERS
-    userDAO.createUser(
-      params["fname"],
-      params["lname"],
-      params["dateOfBirth"],
-      params["email"],
-      params["username"],
-      params["password"]
-    );
-    res.status(201).send("User created successfully");
-  } else {
-    res.status(404).send("Insufficient parameters provided");
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(422).json({ errors: errors.array() });
+    return;
   }
+  const {
+    firstName,
+    lastName,
+    dateOfBirth,
+    email,
+    username,
+    password
+  } = req.query;
+  userDAO.createUser(
+    firstName,
+    lastName,
+    dateOfBirth,
+    email,
+    username,
+    password
+  );
+  res.status(201).send("User created successfully");
 };
+
 exports.delete = (req, res) => {
   userDAO.getUser(req.params.username, result => {
     if (result) {
       userDAO.deleteUser(req.params.username);
       res.status(204).send("User successfully deleted");
     } else {
-      res.status(404).send(`User with username ${req.params.username} not found`);
+      res
+        .status(404)
+        .send(`User with username ${req.params.username} not found`);
     }
   });
+};
+
+exports.validate = type => {
+  switch (type) {
+    case "validateUserInfo": {
+      return [
+        query("firstName", "First name does not exist").exists(),
+        query("lastName", "Last name does not exist").exists(),
+        query("dateOfBirth", "Date of birth does not exist").exists(),
+        query("email", "Email does not exist/invalid")
+          .exists()
+          .isEmail(),
+        query("username", "Username does not exist").exists(),
+        query("password", "Password does not exist").exists()
+      ];
+    }
+  }
 };
