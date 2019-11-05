0ce2f5114541a226bee17b71d2fef6d3c21cfc73
Pablo Estrada <pablof300@gmail.com>
2019-11-05 12:30:20 -0500

    Refactored EventDAO to be async and created an Event controller with its routings

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const userRoute = require("./Routes/UserRoute");
+const eventRoute = require("./Routes/EventRoute");
 const authRoute = require("./Routes/AuthRoutes");
 const config = require("./Config/config.json");
 const database = require("./Database/Database.js");
@@ -7,8 +8,12 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 app.use("/api", userRoute);
+app.use("/api", eventRoute);
 app.use("/auth", authRoute);
 database.connect();
 
