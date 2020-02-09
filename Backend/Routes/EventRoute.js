df3c8fa8ea4f69e3fb426704fb4154bd1338b64c
zeeshanamin3 <zeeshanamin3@gmail.com>
2020-02-08 20:29:04 -0500

    Added DAO and routes for getting events by clubs being followed by the user

--- Backend/Routes/EventRoute.js
+++ Backend/Routes/EventRoute.js
@@ -1,9 +1,11 @@
 const eventController = require('../Controllers/EventController');
 const express = require('express');
 const router = express.Router();
+const passport = require("passport");
 
 router.get('/event', eventController.getAll);
 router.get('/event/:id', eventController.get);
+router.get('/event/following/:userId', passport.authenticate("loggedIn", { session: false }), eventController.getFollowing);
 router.put('/event/:id', eventController.validate('validateEventInfo'), eventController.update);
 router.post('/event', eventController.validate('validateEventInfo'), eventController.create);
 router.delete('/event/:id', eventController.delete);
--- Backend/Routes/UserRoute.js
+++ Backend/Routes/UserRoute.js
@@ -9,7 +9,7 @@ const passport = require("passport");
 router.get("/user", passport.authenticate("loggedIn", { session: false }), userController.getAll);
 router.get("/user/:id", passport.authenticate("loggedIn", { session: false }), userController.get);
 router.put("/user/:id", passport.authenticate("loggedIn", { session: false }), userController.validate("validateUserInfo"), userController.update);
-router.post("/user", passport.authenticate("loggedIn", { session: false }), userController.validate("validateUserInfo"), userController.create);
+router.post("/user", userController.validate("validateUserInfo"), userController.create);
 router.delete("/user/:id", passport.authenticate("loggedIn", { session: false }), userController.delete);
 
 module.exports = router;
