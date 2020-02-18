e318662fc96ce1882fa503ded974655d05a42c17
Pablo Estrada <pablof300@gmail.com>
2020-02-17 20:16:39 -0500

    Added UserApi client

--- Frontend/screens/SignupScr.js
+++ Frontend/screens/SignupScr.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   AsyncStorage,
   Dimensions,
@@ -12,101 +12,91 @@ import {
   TextInput,
   TouchableOpacity,
   View
-} from 'react-native';
-import {API} from '../util/API'
+} from "react-native";
+import AuthApi from "../api/AuthApi";
+import UserApi from "../api/UserApi";
 
 export default class SignupScr extends React.Component {
   static navigationOptions = {
     header: null
-  }
+  };
 
   constructor(props) {
     super(props);
     this.state = {
-      firstName: '',
-      lastName: '',
-      major: '',
-      classYear: '',
-      username: '',
-      password: '',
-      verifyPassword: '',
+      firstName: "",
+      lastName: "",
+      major: "",
+      classYear: "",
+      username: "",
+      password: "",
+      verifyPassword: "",
       triedSubmitting: false
-    }
+    };
   }
-
 
   // Renders Error text if name field input is incorrect
-  errorName = (input) => {
-    if (input == '' || !(/^[a-zA-Z()]+$/.test(input))) {
-      return (
-        <Text style={styles.error}>Please enter a valid name</Text>
-      );
+  errorName = input => {
+    if (input == "" || !/^[a-zA-Z()]+$/.test(input)) {
+      return <Text style={styles.error}>Please enter a valid name</Text>;
     }
-  }
+  };
 
   // Validates username otherwise renders error
   errorUsername = () => {
-    if (this.state.username == '' || !(this.state.username.endsWith('@ufl.edu'))) {
-      return (
-        <Text style={styles.error}>Please enter a valid username</Text>
-      );
+    if (
+      this.state.username == "" ||
+      !this.state.username.endsWith("@ufl.edu")
+    ) {
+      return <Text style={styles.error}>Please enter a valid username</Text>;
     }
-  }
+  };
 
   // Validates password otherwise renders error
   errorPassword = () => {
-    if (this.state.password == '' || this.state.password.length < 6) {
-      return (
-        <Text style={styles.error}>Please enter a valid password</Text>
-      );
+    if (this.state.password == "" || this.state.password.length < 6) {
+      return <Text style={styles.error}>Please enter a valid password</Text>;
     } else if (this.state.password !== this.state.verifyPassword) {
-      return (
-        <Text style={styles.error}>Passwords don't match</Text>
-      );
+      return <Text style={styles.error}>Passwords don't match</Text>;
     }
-  }
+  };
 
   signupHandler = async () => {
     this.setState({
       triedSubmitting: true
     });
-
-    await API.post('/api/user', {
-      name: {
-        first: this.state.firstName,
-        last: this.state.lastName
-      },
-      dob: '2000-01-01',
-      email: this.state.username,
-      username: this.state.username,
-      password: this.state.password
-    });
-
-    const resp = await API.post('/auth/login', {
-      username: this.state.username,
-      password: this.state.password
-    });
-    const token = resp.data.token;
-
-    await AsyncStorage.setItem('userToken', token);
-    console.log('New user added');
-    await this.props.navigation.navigate('App');
-  }
-
+    await UserApi.createUser(
+      { first: this.state.firstName, last: this.state.lastName },
+      this.state.username,
+      this.state.password
+    );
+    let authResponse = await AuthApi.authenticate(
+      this.state.username,
+      this.state.password
+    );
+    if (authResponse.token) {
+      await this.props.navigation.navigate("App");
+    } else {
+      console.log("Unable to add new user");
+    }
+  };
 
   render() {
     return (
       <SafeAreaView style={styles.container}>
-        <ScrollView contentContainerStyle={{ flex: 1, display: 'flex' }}>
+        <ScrollView contentContainerStyle={{ flex: 1, display: "flex" }}>
           <Text style={styles.header}>Sign Up</Text>
-          <KeyboardAvoidingView style={styles.formContainer} behavior='padding' keyboardVerticalOffset={50}>
-
+          <KeyboardAvoidingView
+            style={styles.formContainer}
+            behavior="padding"
+            keyboardVerticalOffset={50}
+          >
             {/* First Name */}
             <TextInput
               style={styles.inputFieldText}
-              placeholderTextColor={'#8E8E93'}
+              placeholderTextColor={"#8E8E93"}
               placeholder="First Name"
-              onChangeText={(text) => this.setState({ firstName: text })}
+              onChangeText={text => this.setState({ firstName: text })}
               value={this.state.firstName}
             ></TextInput>
             {this.state.triedSubmitting && this.errorName(this.state.firstName)}
@@ -114,9 +104,9 @@ export default class SignupScr extends React.Component {
             {/* Last Name */}
             <TextInput
               style={styles.inputFieldText}
-              placeholderTextColor={'#8E8E93'}
+              placeholderTextColor={"#8E8E93"}
               placeholder="Last Name"
-              onChangeText={(text) => this.setState({ lastName: text })}
+              onChangeText={text => this.setState({ lastName: text })}
               value={this.state.lastName}
             ></TextInput>
             {this.state.triedSubmitting && this.errorName(this.state.lastName)}
@@ -127,17 +117,30 @@ export default class SignupScr extends React.Component {
                 selectedValue={this.state.major}
                 style={{ flex: 1 }}
                 itemStyle={styles.pickerDropdown}
-                mode={'dialog'}
+                mode={"dialog"}
                 onValueChange={(itemValue, itemIndex) =>
                   this.setState({ major: itemValue })
-                }>
+                }
+              >
                 {/* Need to replace this with a separate major select view */}
-                <Picker.Item label="Computer Science" value="computer science" />
-                <Picker.Item label="Business Administration" value="business administration" />
-                <Picker.Item label="Mechanical Engineering" value="mech engineering" />
+                <Picker.Item
+                  label="Computer Science"
+                  value="computer science"
+                />
+                <Picker.Item
+                  label="Business Administration"
+                  value="business administration"
+                />
+                <Picker.Item
+                  label="Mechanical Engineering"
+                  value="mech engineering"
+                />
                 <Picker.Item label="Architecture" value="architecture" />
                 <Picker.Item label="Anthropology" value="anthropology" />
-                <Picker.Item label="Chemical Engineering" value="chem engineering" />
+                <Picker.Item
+                  label="Chemical Engineering"
+                  value="chem engineering"
+                />
               </Picker>
             </View>
 
@@ -147,10 +150,11 @@ export default class SignupScr extends React.Component {
                 style={{ flex: 1 }}
                 selectedValue={this.state.classYear}
                 itemStyle={styles.pickerDropdown}
-                mode={'dialog'}
+                mode={"dialog"}
                 onValueChange={(itemValue, itemIndex) =>
                   this.setState({ classYear: itemValue })
-                }>
+                }
+              >
                 {/* Need to change this so that years are calculated dynamically */}
                 <Picker.Item label="2019" value="2019" />
                 <Picker.Item label="2020" value="2020" />
@@ -164,9 +168,9 @@ export default class SignupScr extends React.Component {
             {/* username */}
             <TextInput
               style={styles.inputFieldText}
-              placeholderTextColor={'#8E8E93'}
+              placeholderTextColor={"#8E8E93"}
               placeholder="Username (UF email)"
-              onChangeText={(text) => this.setState({ username: text })}
+              onChangeText={text => this.setState({ username: text })}
               value={this.state.username}
             ></TextInput>
             {this.state.triedSubmitting && this.errorUsername()}
@@ -175,9 +179,9 @@ export default class SignupScr extends React.Component {
             <TextInput
               style={styles.inputFieldText}
               secureTextEntry={true}
-              placeholderTextColor={'#8E8E93'}
+              placeholderTextColor={"#8E8E93"}
               placeholder="Password (minimum length 6)"
-              onChangeText={(text) => this.setState({ password: text })}
+              onChangeText={text => this.setState({ password: text })}
               value={this.state.password}
             ></TextInput>
             {this.state.triedSubmitting && this.errorPassword()}
@@ -185,15 +189,13 @@ export default class SignupScr extends React.Component {
             {/* double password */}
             <TextInput
               style={styles.inputFieldText}
-              placeholderTextColor={'#8E8E93'}
+              placeholderTextColor={"#8E8E93"}
               secureTextEntry={true}
               placeholder="Reenter Password"
-              onChangeText={(text) => this.setState({ verifyPassword: text })}
+              onChangeText={text => this.setState({ verifyPassword: text })}
               value={this.state.verifyPassword}
             ></TextInput>
             {this.state.triedSubmitting && this.errorPassword()}
-
-
           </KeyboardAvoidingView>
 
           <TouchableOpacity
@@ -204,35 +206,34 @@ export default class SignupScr extends React.Component {
           </TouchableOpacity>
         </ScrollView>
       </SafeAreaView>
-
     );
   }
 }
 
-const MAX_FIELD_WIDTH = Dimensions.get('screen').width * 3 / 4;
+const MAX_FIELD_WIDTH = (Dimensions.get("screen").width * 3) / 4;
 const bgColor = "#FFF";
 const txtFieldBgColor = "#F4F4F4";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    display: 'flex',
+    display: "flex",
     padding: 20,
-    justifyContent: 'center',
+    justifyContent: "center",
     marginTop: StatusBar.currentHeight,
     backgroundColor: bgColor,
-    flexDirection: 'column'
+    flexDirection: "column"
   },
   error: {
-    color: 'red',
+    color: "red",
     fontSize: 12,
-    paddingHorizontal: 10,
+    paddingHorizontal: 10
   },
   header: {
     fontSize: 22,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginVertical: 5,
-    color: '#636e72',
+    color: "#636e72",
     flex: 1,
     alignSelf: "center"
   },
@@ -266,20 +267,20 @@ const styles = StyleSheet.create({
   signupButton: {
     padding: 10,
     minWidth: MAX_FIELD_WIDTH,
-    backgroundColor: '#ACCBAC',
+    backgroundColor: "#ACCBAC",
     borderWidth: 1,
-    borderColor: '#ACCBAC',
+    borderColor: "#ACCBAC",
     borderRadius: 100,
     marginHorizontal: 10,
     marginVertical: 10,
-    elevation: 3,
+    elevation: 3
   },
   nameField: {
-    marginLeft: 20,
+    marginLeft: 20
   },
   signupButtonTxt: {
     fontSize: 15,
-    color: '#FFF',
-    alignSelf: 'center',
-  },
+    color: "#FFF",
+    alignSelf: "center"
+  }
 });
