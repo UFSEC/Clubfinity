dc53ef5a2866f40ce825a2f806c7898e78d7fb14
Evan Rupert <evan@studioreach.io>
2020-03-01 20:37:46 -0500

    Add home page empty states - Empty state for not following clubs - Empty state for no upcoming club events - Add endpoint to get following events & clubs

--- Frontend/components/DiscoverButton.js
+++ Frontend/components/DiscoverButton.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 
-export default class SignupButton extends Component {
+export default class DiscoverButton extends Component {
 
   constructor(props) {
     super(props);
@@ -13,21 +13,26 @@ export default class SignupButton extends Component {
 
   render() {
     return (
-      <MaterialIcons.Button size={25} backgroundColor="#D4D4D4" onPress={this._eventHandler}>
-        Discover
+      <MaterialIcons.Button
+        borderRadius={50}
+        backgroundColor="#7e947f"
+        textAlign="center"
+        style={styles.button}
+        onPress={this.props.onPress}>
+        <Text style={styles.text}>Discover</Text>
       </MaterialIcons.Button>
     );
-  }
-
-  _eventHandler = () => {
-    // TODO
   }
 }
 
 const styles = StyleSheet.create({
   button: {
+    width: 200,
+    paddingLeft: 0
+  },
+  text: {
     flex: 1,
-    maxWidth: 200,
-    alignSelf: 'center'
+    textAlign: 'center',
+    color: 'white'
   }
-})
+});
