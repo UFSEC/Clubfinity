850d0912f2eaf20104ac54fa3b55b447a231ffc5
David Sadowsky <davidsadowsky0@gmail.com>
2020-03-22 20:17:18 +0000

    Update User.js

--- Backend/Model/User.js
+++ Backend/Model/User.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = new mongoose.Schema({
   name: { first: String, last: String },
   major: String,
-  year: String,
+  year: Number,
   username: String,
   email: String,
   password: String,
