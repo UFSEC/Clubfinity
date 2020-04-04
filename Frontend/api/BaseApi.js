5991478a439b8f8a52d2935a33a58e933c8d4f09
Pablo Estrada <pablof300@gmail.com>
2020-04-03 20:36:57 -0400

    Updated baseURL to correct url

--- Frontend/api/BaseApi.js
+++ Frontend/api/BaseApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 exports.API = axios.create({
-    baseURL: 'http://1d2bd0f2.ngrok.io',
+    baseURL: 'https://clubfinitybackend.azurewebsites.net',
     responseType: "json",
 });
