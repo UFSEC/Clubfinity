4e57d7063dfcedcde46f4a87ace975d17f775ca4
Michel Chalfoun <michelchalfoun@Michels-MacBook-Pro.local>
2020-10-13 01:34:16 -0400

    Added Edit EventsScreen

--- Frontend/screens/EditEvent.js
+++ Frontend/screens/EditEvent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { AsyncStorage, Platform } from 'react-native';
+import { View } from 'react-native';
 import {
   Button,
   Text,
@@ -7,30 +7,23 @@ import {
   Content,
   Form,
   Item,
-  Picker,
   Label,
   Input,
+  Textarea,
 } from 'native-base';
 import { Ionicons } from '@expo/vector-icons';
+import DatePicker from 'react-native-datepicker';
 import colors from '../util/colors';
-import Majors from '../data/Majors';
-import ClassYears from '../data/ClassYears';
-import UserContext from '../util/UserContext';
-import UserApi from '../api/UserApi';
-import EventApi from '../api/EventsApi'
 
-const evData = [
-  {
-    id: 1,
-    name: 'GBM 2',
-    date: '10/8/20',
-    time: '7:00',
-    location: 'Zoom',
-    facebookLink: 'https://www.facebook.com/events/3684185941606080',
-    eventDescription: 'New officers introduction!',
-  },
-];
-
+const evData = {
+  id: 1,
+  name: 'GBM 2',
+  date: '10/8/20',
+  time: '7:00',
+  location: 'Zoom',
+  facebookLink: 'https://www.facebook.com/events/3684185941606080',
+  eventDescription: 'New officers introduction!',
+};
 
 export default class EditEvent extends Component {
   static navigationOptions = {
@@ -64,6 +57,7 @@ export default class EditEvent extends Component {
 
   componentDidMount() {
     const event = evData;
+    console.log(event);
 
     this.setState({
       eventName: event.name,
@@ -73,6 +67,7 @@ export default class EditEvent extends Component {
       facebookLink: event.facebookLink,
       eventDescription: event.eventDescription,
     });
+    console.log(this.state);
   }
 
   editEvent = async () => {
@@ -80,98 +75,66 @@ export default class EditEvent extends Component {
     if (!validRequest.valid) {
       this.setState({
         processingRequest: { status: false, message: '' },
-        errors: { arePresent: true, data: validRequest.errors },
+        errors: { arePresent: true, data: validRequest.errors.data },
       });
       return;
     }
     this.setState({
-      processingRequest: { status: true, message: 'Updating...' },
-      errors: { arePresent: false, data: validRequest.errors },
+      processingRequest: { status: false, message: 'Updating...' },
+      errors: { arePresent: false, data: validRequest.errors.data },
     });
-    const {
-      eventName,
-      selectedDate,
-      selectedTime,
-      location,
-      facebookLink,
-      eventDescription,
-    } = this.state;
-
-    // Set event equal to EventContext
-    const { event, setEvent } = this.context;
-
-    event.eventName = eventName;
-    event.selectedDate = selectedDate;
-    event.selectedTime = selectedTime;
-    event.location = location;
-    event.facebookLink = facebookLink;
-    event.eventDescription = eventDescription;
-
-    // Set response to event update function in EventsApi
-    const updateEventResponse;
-
-    if (updateEventResponse.error) {
-      alert('Unable to update event');
-      console.log(updateEventResponse.error);
-      return;
-    }
-    this.setState({
-      processingRequest: { status: true, message: 'Updated Event!' },
-    });
-    setUser(event);
   };
 
   isRequestValid = () => {
     const {
-      errors,
       eventName,
       selectedDate,
       selectedTime,
       location,
       facebookLink,
       eventDescription,
+      errors,
     } = this.state;
-    const errorsData = { ...errors.data };
+    const errorData = errors.data;
     errorData.eventName = eventName === '' || eventName.length < 3;
     errorData.selectedDate = selectedDate === '';
     errorData.selectedTime = selectedTime === '';
     errorData.location = location === '' || location.length < 3;
     errorData.facebookLink = !!facebookLink && !this.validURL(facebookLink);
     errorData.eventDescription = eventDescription.length > 280;
-
     let validRequest = true;
-    Object.keys(errorsData).forEach((input) => {
-      if (errorsData[input]) {
+    Object.keys(errorData).forEach((input) => {
+      if (errorData[input] && input !== 'data' && input !== 'arePresent') {
         validRequest = false;
       }
     });
-    return { valid: validRequest, errors: errorsData };
+    return { valid: validRequest, errors };
   };
 
-  setMajor = (major) => {
-    this.setState({ major });
-  };
-
-  setYear = (year) => {
-    this.setState({ classYear: year });
+  validURL = (str) => {
+    const pattern = new RegExp(
+      '^(https?:\\/\\/)?' // protocol
+      + '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' // domain name
+      + '((\\d{1,3}\\.){3}\\d{1,3}))' // OR ip (v4) address
+      + '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' // port and path
+      + '(\\?[;&a-z\\d%_.~+=-]*)?' // query string
+        + '(\\#[-a-z\\d_]*)?$',
+      'i',
+    ); // fragment locator
+    return !!pattern.test(str) && str.toLowerCase().includes('facebook');
   };
 
   render() {
     const {
       processingRequest,
       errors,
-      firstName,
-      lastName,
-      username,
-      major,
-      classYear,
+      eventName,
+      selectedDate,
+      selectedTime,
+      location,
+      facebookLink,
+      eventDescription,
     } = this.state;
-    const majorCategories = Majors.map((s) => (
-      <Picker.Item value={s.value} label={s.label} />
-    ));
-    const classYearCategories = ClassYears.map((s) => (
-      <Picker.Item value={s.value} label={s.label} />
-    ));
     return (
       <Container>
         <Content>
@@ -185,137 +148,256 @@ export default class EditEvent extends Component {
             }}
           >
             <Item
-              style={{
-                width: '95%',
-                height: 45,
-                marginBottom: '5%',
-                marginTop: '7%',
-              }}
-              stackedLabel
+              fixedLabel
+              style={{ width: '95%', height: 45, marginBottom: '5%' }}
             >
               <Label
                 style={{
                   color:
-                    errors.arePresent && errors.data.username
+                    errors.arePresent && errors.data.eventName
                       ? colors.error
                       : colors.grayScale10,
                 }}
               >
-                Username
+                Event Name
               </Label>
+              {console.log(errors)}
               <Input
-                onChangeText={(value) => this.setState({ username: value })}
+                onChangeText={(value) => this.setState({ eventName: value })}
+                style={{ textAlign: 'right' }}
+                height="50%"
+                placeholderTextColor={colors.error}
+                placeholder={
+                  errors.arePresent && errors.data.eventName
+                    ? 'Invalid event name'
+                    : ''
+                }
               >
-                {username}
+                {eventName}
               </Input>
+
             </Item>
             <Item
+              fixedLabel
               style={{ width: '95%', height: 45, marginBottom: '5%' }}
-              stackedLabel
             >
               <Label
                 style={{
                   color:
-                    errors.arePresent && errors.data.firstName
+                    errors.arePresent && errors.data.date
                       ? colors.error
                       : colors.grayScale10,
                 }}
               >
-                First name
+                Date
               </Label>
-              <Input
-                onChangeText={(value) => this.setState({ firstName: value })}
-              >
-                {firstName}
-              </Input>
+              <DatePicker
+                style={{ width: 200 }}
+                date={selectedDate}
+                mode="date"
+                placeholder={errors.arePresent && errors.data.selectedDate
+                  ? 'Invalid date'
+                  : 'Select a Date'}
+                format="MMM Do YYYY"
+                minDate="01-01-2020"
+                confirmBtnText="Set Date"
+                cancelBtnText="Cancel"
+                showIcon={false}
+                customStyles={{
+                  dateIcon: {
+                    position: 'absolute',
+                    left: 0,
+                    top: 4,
+                    marginLeft: 0,
+                  },
+                  dateInput: {
+                    marginLeft: 50,
+                    borderColor: 'white',
+                  },
+                  dateText: {
+                    fontSize: 17,
+                    color: colors.grayScale10,
+                  },
+                  placeholderText: {
+                    fontSize: 17,
+                    color:
+                      errors.arePresent && errors.data.eventName
+                        ? colors.error
+                        : colors.grayScale10,
+                  },
+                }}
+                onDateChange={(date) => this.setState({ selectedDate: date })}
+              />
+              <Ionicons
+                name="md-arrow-dropdown"
+                size={20}
+                style={{ paddingTop: '1%' }}
+              />
             </Item>
 
             <Item
+              fixedLabel
               style={{ width: '95%', height: 45, marginBottom: '5%' }}
-              stackedLabel
             >
               <Label
                 style={{
                   color:
-                    errors.arePresent && errors.data.lastName
+                    errors.arePresent && errors.data.date
                       ? colors.error
                       : colors.grayScale10,
                 }}
               >
-                Last name
+                Time
+              </Label>
+              <DatePicker
+                style={{ width: 200 }}
+                date={selectedTime}
+                mode="time"
+                placeholder={errors.arePresent && errors.data.selectedTime
+                  ? 'Invalid time'
+                  : 'Select a Time'}
+                format="hh:mm A"
+                confirmBtnText="Set Time"
+                cancelBtnText="Cancel"
+                minuteInterval={10}
+                showIcon={false}
+                customStyles={{
+                  dateIcon: {
+                    position: 'absolute',
+                    left: 0,
+                    top: 4,
+                    marginLeft: 0,
+                  },
+                  dateInput: {
+                    marginLeft: 50,
+                    borderColor: 'white',
+                  },
+                  dateText: {
+                    fontSize: 17,
+                    color: colors.grayScale10,
+                  },
+                  placeholderText: {
+                    fontSize: 17,
+                    color:
+                        errors.arePresent && errors.data.eventName
+                          ? colors.error
+                          : colors.grayScale10,
+                  },
+                }}
+                onDateChange={(time) => this.setState({ selectedTime: time })}
+              />
+              <Ionicons
+                name="md-arrow-dropdown"
+                size={20}
+                style={{ paddingTop: '1%' }}
+              />
+            </Item>
+            <Item
+              fixedLabel
+              style={{ width: '95%', height: 45, marginBottom: '5%' }}
+            >
+              <Label
+                style={{
+                  color:
+                    errors.arePresent && errors.data.location
+                      ? colors.error
+                      : colors.grayScale10,
+                }}
+              >
+                Location
               </Label>
               <Input
-                onChangeText={(value) => this.setState({ lastName: value })}
+                onChangeText={(value) => this.setState({ location: value })}
+                style={{ textAlign: 'right' }}
+                placeholderTextColor={colors.error}
+                placeholder={
+                  errors.arePresent && errors.data.location
+                    ? 'Invalid event location'
+                    : ''
+                }
               >
-                {lastName}
+                {location}
               </Input>
             </Item>
-            <Item
-              picker
-              fixedLabel
-              style={{ width: '95%', marginBottom: '5%' }}
-            >
-              <Label
+            {errors.arePresent && errors.data.facebookLink ? (
+              <Item
+                fixedLabel
+                style={{ width: '95%', height: 45, marginBottom: '5%' }}
+              >
+                <Label style={{ color: colors.error }}>Facebook</Label>
+                <Input
+                  onChangeText={(value) => this.setState({ facebookLink: value })}
+                  style={{ textAlign: 'right' }}
+                  placeholderTextColor={colors.error}
+                  placeholder="Invalid Link"
+                />
+              </Item>
+            ) : (
+              <Item
+                fixedLabel
+                style={{ width: '95%', height: 45, marginBottom: '5%' }}
+              >
+                <Label>Facebook Link</Label>
+                <Input
+                  onChangeText={(value) => this.setState({ facebookLink: value })}
+                  style={{ textAlign: 'right' }}
+                  placeholder=""
+                  height="50%"
+                >
+                  {facebookLink}
+                </Input>
+              </Item>
+            )}
+            {errors.arePresent && errors.data.eventDescription ? (
+              <View
                 style={{
-                  color:
-                    errors.arePresent && errors.data.major
-                      ? colors.error
-                      : colors.grayScale10,
+                  width: '100%',
                 }}
               >
-                Major
-              </Label>
-              <Picker
-                mode="dropdown"
-                style={{ width: undefined }}
-                placeholderStyle={{ color: colors.grayScale10 }}
-                placeholder="Select club category"
-                selectedValue={major}
-                onValueChange={this.setMajor}
-              >
-                {majorCategories}
-              </Picker>
-              {Platform.OS === 'ios' ? (
-                <Ionicons
-                  name="md-arrow-dropdown"
-                  size={20}
-                  style={{ paddingTop: '1%' }}
+                <Textarea
+                  rowSpan={5}
+                  bordered
+                  borderColor={colors.error}
+                  placeholderTextColor={colors.error}
+                  placeholder="Invalid description*"
+                  value={eventDescription}
+                  onChangeText={(value) => this.setState({ eventDescription: value })}
+                  style={{
+                    alignSelf: 'center',
+                    width: '95%',
+                    paddingBottom: '5%',
+                    marginLeft: '4%',
+                  }}
                 />
-              ) : null}
-            </Item>
-            <Item
-              picker
-              fixedLabel
-              style={{ width: '95%', marginBottom: '5%' }}
-            >
-              <Label
+                <Text
+                  style={{
+                    color: colors.error,
+                    fontSize: 14,
+                    alignSelf: 'center',
+                    width: '95%',
+                    paddingBottom: '5%',
+                    marginLeft: '4%',
+                  }}
+                >
+                  Must be less than 280 characters
+                </Text>
+              </View>
+            ) : (
+              <Textarea
+                rowSpan={5}
+                bordered
+                placeholder="Description (Max 280 characters*)"
+                value={eventDescription}
+                onChangeText={(value) => this.setState({ eventDescription: value })}
+                placeholderStyle={{ color: colors.grayScale10 }}
                 style={{
-                  color:
-                    errors.arePresent && errors.data.classYear
-                      ? colors.error
-                      : colors.grayScale10,
+                  alignSelf: 'center',
+                  width: '95%',
+                  paddingBottom: '5%',
+                  marginLeft: '4%',
                 }}
-              >
-                Class Year
-              </Label>
-              <Picker
-                mode="dropdown"
-                style={{ width: undefined }}
-                placeholderStyle={{ color: colors.grayScale10 }}
-                placeholder="Select class year"
-                selectedValue={classYear.toString()}
-                onValueChange={this.setYear}
-              >
-                {classYearCategories}
-              </Picker>
-              {Platform.OS === 'ios' ? (
-                <Ionicons
-                  name="md-arrow-dropdown"
-                  size={20}
-                  style={{ paddingTop: '1%' }}
-                />
-              ) : null}
-            </Item>
+              />
+            )}
           </Form>
           <Button
             style={{
@@ -325,15 +407,13 @@ export default class EditEvent extends Component {
               justifyContent: 'center',
               alignItems: 'center',
               marginRight: '1%',
-              marginBottom: '5%',
-              marginTop: '5%',
             }}
-            onPress={this.editProfile}
+            onPress={this.editEvent}
           >
             <Text style={{ alignSelf: 'center' }}>
               {processingRequest.status
                 ? processingRequest.message
-                : 'Update Profile'}
+                : 'Update Event'}
             </Text>
           </Button>
         </Content>
