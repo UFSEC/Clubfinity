2ad0a37bfe3c0a89664f078406dab9942dcfd92f
liapassaglia <liapassaglia@gmail.com>
2020-10-18 18:06:42 -0400

    Fixed thumbnail sizing, touchable list items, and added icon to nav bar

--- Frontend/screens/AdminList.js
+++ Frontend/screens/AdminList.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import { FlatList } from 'react-native';
+import { View, FlatList, TouchableOpacity } from 'react-native';
 import {
   Container,
   Text,
-  Header,
-  Button,
   Thumbnail,
-  H2,
   ListItem,
   Content,
   Left,
-  Right,
 } from 'native-base';
 import { Ionicons } from '@expo/vector-icons';
 import DefaultPic from '../assets/images/profile-icon.png';
@@ -21,6 +17,17 @@ export default class AdminList extends React.Component {
       headerStyle: { backgroundColor: '#7e947f' },
       headerTitleStyle: { color: '#ecf0f1', letterSpacing: 2 },
       headerTintColor: 'white',
+      headerRight: (
+        <View style={{ flexDirection: 'row', justifyContent: 'flex-end', paddingRight: 15 }}>
+          <TouchableOpacity>
+            <Ionicons
+              name="md-add"
+              size={30}
+              color="white"
+            />
+          </TouchableOpacity>
+        </View>
+      ),
     };
 
     constructor() {
@@ -34,36 +41,28 @@ export default class AdminList extends React.Component {
       const { admins } = this.state;
       return (
         <Container>
-          <Header>
-            <Left>
-              <H2 style={{ marginLeft: 10 }}>Admins</H2>
-            </Left>
-            <Right>
-              <Button transparent>
-                <Ionicons
-                  name="md-add"
-                  size={20}
-                  style={{ paddingTop: '7%' }}
-                />
-              </Button>
-            </Right>
-          </Header>
+          <Text style={{
+            marginTop: 10, marginLeft: 10, fontSize: 25, fontWeight: 'bold',
+          }}
+          >
+            Admins
+          </Text>
           <Content style={{ margin: 10 }}>
             <FlatList
               data={admins}
               renderItem={({ item }) => (
                 <ListItem noIndent>
-                  <Left>
-                    <Thumbnail size={5} source={DefaultPic} />
-                    <Text style={{ fontSize: 20, margin: 10 }}>{item.name}</Text>
-                  </Left>
-                  <Right>
-                    <Ionicons
-                      name="md-arrow-forward"
-                      size={20}
-                      style={{ paddingTop: '7%' }}
-                    />
-                  </Right>
+                  <TouchableOpacity>
+                    <Left>
+                      <Thumbnail small source={DefaultPic} />
+                      <Text style={{ fontSize: 16, margin: 5 }}>{item.name}</Text>
+                      <Ionicons
+                        name="md-arrow-forward"
+                        size={25}
+                        style={{ marginLeft: 100, marginTop: 5 }}
+                      />
+                    </Left>
+                  </TouchableOpacity>
                 </ListItem>
               )}
               keyExtractor={(item) => item.name}
