63680fd8db9b9d57e8828dcb4c21959cd8644163
Evan Rupert <evan@studioreach.io>
2020-10-19 18:30:02 -0400

    Divide list into 'Upcoming Events' and 'Past Events' - Create empty state when there are no events
    [#239]

--- Frontend/screens/EventList.js
+++ Frontend/screens/EventList.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import {
   AsyncStorage,
   View,
-  FlatList,
+  SectionList,
+  Text,
 } from 'react-native';
-import { List } from 'native-base';
+import { DateTime } from 'luxon';
 
 import EventsApi from '../api/EventsApi';
 import Row from '../components/Row';
@@ -20,7 +21,8 @@ class EventList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      events: [],
+      pastEvents: [],
+      upcomingEvents: [],
     };
   }
 
@@ -30,29 +32,57 @@ class EventList extends Component {
     const bearerToken = await AsyncStorage.getItem('userToken');
 
     const events = await EventsApi.getForClub(bearerToken, club._id);
-    this.setState({ events });
+    const now = DateTime.local();
+    const pastEvents = events.filter((event) => event.date < now);
+    const upcomingEvents = events.filter((event) => event.date > now);
+
+    this.setState({ pastEvents, upcomingEvents });
   }
 
+  renderSectionHeader = (section) => (
+    <Text
+      style={{
+        fontWeight: 'bold',
+        fontSize: 18,
+        marginBottom: 15,
+        backgroundColor: 'white',
+      }}
+    >
+      {section.title}
+    </Text>
+  )
+
+  renderEmpty = () => (
+    <Text style={{ color: 'rgba(0, 0, 0, 0.50)', textAlign: 'center' }}>No Upcoming or Past Events</Text>
+  )
+
   render() {
-    const { events } = this.state;
+    const { upcomingEvents, pastEvents } = this.state;
+
+    const listData = [];
+    if (upcomingEvents.length > 0) {
+      listData.push({ title: 'Upcoming Events', data: upcomingEvents });
+    }
+
+    if (pastEvents.length > 0) {
+      listData.push({ title: 'Past Events', data: pastEvents });
+    }
 
     return (
-      <View style={{ flex: 1, marginTop: 20 }}>
-        <List>
-          <FlatList
-            style={{ height: '100%' }}
-            data={events}
-            keyExtractor={(event) => event._id}
-            numColumns={1}
-            renderItem={({ item }) => (
-              <Row
-                date={item.date.toFormat('MMM dd yyyy')}
-                text={item.name}
-                handler={() => { }}
-              />
-            )}
-          />
-        </List>
+      <View style={{ flex: 1, paddingVertical: 20, paddingHorizontal: 30 }}>
+        <SectionList
+          sections={listData}
+          keyExtractor={(event) => event._id}
+          renderItem={({ item }) => (
+            <Row
+              date={item.date.toFormat('MMM dd yyyy')}
+              text={item.name}
+              handler={() => { }}
+            />
+          )}
+          renderSectionHeader={({ section }) => this.renderSectionHeader(section)}
+          ListEmptyComponent={this.renderEmpty}
+        />
       </View>
     );
   }
