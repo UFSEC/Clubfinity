87be4b7a72b19fd716322f5a95814cdb5c412fbf
Pablo Estrada <pablof300@gmail.com>
2021-03-04 20:34:03 -0500

    Added font size fix

--- Frontend/screens/ClubScr.js
+++ Frontend/screens/ClubScr.js
@@ -401,7 +401,7 @@ export default class ClubScr extends React.Component {
                         alignSelf: 'center',
                         opacity: 0.7,
                         marginBottom: '5%',
-                        fontSize: '14',
+                        fontSize: 14,
                       }}
                     >
                       There are no announcements for this club.
